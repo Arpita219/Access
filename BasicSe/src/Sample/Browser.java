c60f39d956a73d1bd5d6508a1f68a0edba9aa082
Arpita219 <127617289+Arpita219@users.noreply.github.com>
2023-05-10 18:16:25 +0530

    1st commit

---
+++ BasicSe/src/Sample/Browser.java
@@ -0,0 +1,19 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		String key="webdriver.edge.driver";
+		String value="./msedgedriver.exe";
+		System.setProperty(key, value);
+		WebDriver driver=new EdgeDriver();
+	}
+
+}
